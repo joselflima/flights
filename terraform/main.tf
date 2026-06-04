e718e6cfad1d16e57638668ab0ed383d7106a5ee
joselflima <lucasferlima8@gmail.com>
2026-06-03 23:11:30 -0300

    feat: adding terraform config for google cloud

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "ev-elt"
+  region  = "us-east4"
+}
+
+resource "google_storage_bucket" "raw_data" {
+  name     = "olist-pipeline-raw-data"
+  location = "US"
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 90
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "olist" {
+  dataset_id = "olist_pipeline"
+  location   = "US"
+}
