cb94671b938cb9b5756832d07975c2882080c196
joselflima <lucasferlima8@gmail.com>
2026-06-09 18:11:43 -0300

    chore: adjust Terraform settings to add external tables in data warehouse

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,19 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "google" {
+  project = "ev-elt"
+  region  = "us-east4"
+}
+
+provider "google-beta" {
   project = "ev-elt"
   region  = "us-east4"
 }
@@ -28,7 +37,53 @@ resource "google_storage_bucket" "raw_data" {
   }
 }
 
-resource "google_bigquery_dataset" "flights" {
-  dataset_id = "flight_pipeline"
+# === Datasets per layer ===
+
+resource "google_bigquery_dataset" "bronze" {
+  dataset_id = "bronze"
   location   = "US"
+}
+
+resource "google_bigquery_dataset" "silver" {
+  dataset_id = "silver"
+  location   = "US"
+}
+
+resource "google_bigquery_dataset" "gold" {
+  dataset_id = "gold"
+  location   = "US"
+}
+
+# === External tables in BigQuery ===
+
+resource "google_bigquery_table" "bronze_business" {
+  dataset_id          = google_bigquery_dataset.bronze.dataset_id
+  table_id            = "business"
+  deletion_protection = false
+
+  external_data_configuration {
+    source_format = "CSV"
+    autodetect    = true
+    source_uris   = ["gs://${google_storage_bucket.raw_data.name}/bronze/business.csv"]
+  }
+}
+
+resource "google_bigquery_table" "bronze_economy" {
+  dataset_id          = google_bigquery_dataset.bronze.dataset_id
+  table_id            = "economy"
+  deletion_protection = false
+
+  external_data_configuration {
+    source_format = "CSV"
+    autodetect    = true
+    source_uris   = ["gs://${google_storage_bucket.raw_data.name}/bronze/economy.csv"]
+  }
+}
+
+# === Dataform config ===
+
+resource "google_dataform_repository" "pipeline" {
+  provider = google-beta
+  name     = "flight_pipeline"
+  region   = "us-east4"
 }
